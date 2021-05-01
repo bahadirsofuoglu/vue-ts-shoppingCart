f8b239beb65fc0b806f12a46ce8b92e26f2fb469
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-05-01 21:09:52 +0300

    appButton changes

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,12 +1,22 @@
 <template>
-  <b-button :type="`is-${type}`">{{ label }}</b-button>
+  <b-button
+    :type="`is-${type}`"
+    :size="`is-${size}`"
+    :icon-left="icon"
+    v-bind="$attrs"
+    @click="$emit('click')"
+    >{{ cartLabel ? cartLabel : buttonlabel }}</b-button
+  >
 </template>
 <script lang="ts">
 import { Prop, Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class Home extends Vue {
-  @Prop() label!: string
+  @Prop() buttonLabel!: string
+  @Prop() cartLabel!: number
   @Prop({ default: 'danger' }) type!: string
+  @Prop({ default: 'medium' }) size!: string
+  @Prop() icon!: string
 }
 </script>
