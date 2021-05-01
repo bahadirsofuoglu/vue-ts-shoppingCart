b5d420576638ec2478f5c63bf6d51896fbb330ad
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-05-01 23:58:31 +0300

    footer slot added to appProductCard

--- src/components/AppProductCard.vue
+++ src/components/AppProductCard.vue
@@ -8,7 +8,10 @@
     <div class="card-content">
       <b-image :src="src" alt="A random image" ratio="6by4"></b-image>
     </div>
-    <footer class="card-footer" center>
+    <footer class="card-footer" center v-if="$slots.footerss">
+      <slot name="footerss" />
+    </footer>
+    <!--  <footer class="card-footer" center>
       <AppButton
         class="card-footer-item"
         buttonLabel="Add To Cart"
@@ -16,7 +19,7 @@
         type="outline"
         size="small"
       />
-    </footer>
+    </footer> -->
   </div>
 </template>
 <script lang="ts">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,19 +5,35 @@
         v-for="item in 60"
         :key="item"
         style="margin-left:15px; margin-top:30px"
-      />
+      >
+        <template v-slot:footerss>
+          <AppButton
+            buttonLabel="Add To Cart"
+            icon="plus"
+            type="outline"
+            size="small"
+            class="card-footer-item"
+            @click="clickMe"
+          />
+        </template>
+      </AppProductCard>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-
+import AppButton from '@/components/AppButton.vue'
 import AppProductCard from '@/components/AppProductCard.vue'
 @Component({
   components: {
-    AppProductCard
+    AppProductCard,
+    AppButton
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public clickMe (): void {
+    console.log('clicked')
+  }
+}
 </script>
